903da3fa0f38fd098c17ae78aca2f5bd3a8a4d6f
LIFE <dkrshw65@gmail.com>
2020-07-21 03:22:11 -0400

    issue resolved

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,8 @@ require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 })
 
+console.log(process.env.GATSBY_CLIENT_EMAIL, ' env variables')
+
 module.exports = {
   siteMetadata: {
     title: `Github User Finder`,
